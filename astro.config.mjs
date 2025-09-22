3cf465c5d48f647fa5db461d7483a1346804fc08
Bruno Camarneiro <camarneirobruno@gmail.com>
2025-09-22 14:33:31 +0100

    Update site URL and base path in Astro configuration for blog deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://cyber-meia.github.io',
-	base: '/',
+	site: 'https://cyber-meia.github.io/blog',
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
